039333e109fb0cddf386645f46e1212654867369
Gooey <caethan@gmail.com>
2022-08-30 17:48:50 -0500

    BaseStuffAdded

--- Assets/Scripts/Collectibles/HealthIncrease.cs
+++ Assets/Scripts/Collectibles/HealthIncrease.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class HealthIncrease : CollectibleBase
 {
-    [SerializeField] int _healthAdded
+    [SerializeField] int _healthAdded = 1;
+
+    protected override void Collect(Player player)
+    {
+        player.IncreaseHealth(_healthAdded);
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
 [RequireComponent(typeof(TankController))]
 public class Player : MonoBehaviour
 {
@@ -24,14 +23,14 @@ public class Player : MonoBehaviour
     public void IncreaseHealth(int amount)
     {
         _currentHealth += amount;
-        _currentHealth = Mathf.Clamp(_currentHealth,0,_maxHealth);
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
         Debug.Log("Player Health: " + _currentHealth + " / " + _maxHealth);
     }
     public void DecreaseHealth(int amount)
     {
         _currentHealth -= amount;
         Debug.Log("Player Health: " + _currentHealth + " / " + _maxHealth);
-        if(_currentHealth <= 0)
+        if (_currentHealth <= 0)
         {
             Kill();
         }
@@ -41,19 +40,6 @@ public class Player : MonoBehaviour
         gameObject.SetActive(false);
         //boom
         //bang
-=======
-public class Player : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
->>>>>>> main
-    }
 }
